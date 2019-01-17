3b6b90c841ed2ab550339bf106294e8b61850218
Jeongdasilver <alal8272@gmail.com>
2019-01-17 17:58:54 +0900

    ADD PracticeFCM

---
+++ PracticeFCM/app/src/main/java/com/example/daeun/practicefcm/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.daeun.practicefcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
